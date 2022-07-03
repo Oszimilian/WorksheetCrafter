ac5334db62a16a09ccefb180fadad485fcbd5774
maximilian <maximilian@DESKTOP-A4IQKPO.localdomain>
2022-07-03 23:42:48 +0200

    First set up for the WorksheetCrafter

---
+++ WorksheetCrafter/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <gtk/gtk.h>
+#include <time.h>
+#include <setjmp.h>
+#include <string.h>
+#include "hpdf.h"
+
+jmp_buf env;
+
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
